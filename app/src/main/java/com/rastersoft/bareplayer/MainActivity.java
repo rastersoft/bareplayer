0cefb7e5a30ba5b8007d78755dfa2572cf3a99f6
Sergio Costas <raster@rastersoft.com>
2017-05-09 00:06:14 +0200

    Added support for landscape orientation

--- app/src/main/java/com/rastersoft/bareplayer/MainActivity.java
+++ app/src/main/java/com/rastersoft/bareplayer/MainActivity.java
@@ -5,18 +5,27 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 
 import java.io.File;
+
+import static android.view.Surface.ROTATION_0;
+import static android.view.Surface.ROTATION_180;
+import static android.view.Surface.ROTATION_270;
+import static android.view.Surface.ROTATION_90;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     };
     private SharedPreferences prefs;
     private String musicPath;
+    private int currentOrientation;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,14 +72,41 @@ public class MainActivity extends AppCompatActivity {
         this.verifyStoragePermissions(this);
         this.prefs = this.getPreferences(Context.MODE_PRIVATE);
         this.musicPath = prefs.getString("musicpath",Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath());
+        Configuration newConfig = this.getResources().getConfiguration();
+        Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        switch (display.getRotation()) {
+            case ROTATION_0:
+                this.currentOrientation = 0;
+                break;
+            case ROTATION_90:
+                this.currentOrientation = 1;
+                break;
+            case ROTATION_180:
+                this.currentOrientation = 2;
+                break;
+            case ROTATION_270:
+                this.currentOrientation = 3;
+                break;
+            default:
+                this.currentOrientation = 0;
+                break;
+        }
+        Log.e("Orientacion",String.format("%1$d; %2$d",this.currentOrientation,newConfig.screenLayout));
+
     }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+    }
 
     public void randomSongClicked(View view) {
 
         Intent myIntent = new Intent(this, MainPlayerActivity.class);
         myIntent.putExtra("launch_mode", 1); //Optional parameters
         myIntent.putExtra("music_path",this.musicPath);
+        myIntent.putExtra("orientation",this.currentOrientation);
         this.startActivity(myIntent);
     }
 
@@ -77,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
         Intent myIntent = new Intent(this, MainPlayerActivity.class);
         myIntent.putExtra("launch_mode", 2);
         myIntent.putExtra("music_path",this.musicPath);
+        myIntent.putExtra("orientation",this.currentOrientation);
         this.startActivity(myIntent);
     }
 
